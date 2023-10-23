a178a3f331b4595bdecb4768e7d1dd4b5934eb0c
D:/Program Files/DevTools/Git/jxk <1286783182@qq.com>
2023-10-24 00:51:03 +0800

    提交

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,17 +1,21 @@
 // a melonJS data manifest
 const DataManifest = [
-
-    /* Bitmap Text */
-    {
-        name: "PressStart2P",
-        type: "image",
-        src:  "/data/fnt/PressStart2P.png"
-    },
-    {
-        name: "PressStart2P",
-        type: "binary",
-        src: "/data/fnt/PressStart2P.fnt"
-    }
+  /* Bitmap Text */
+  {
+    name: "PressStart2P",
+    type: "image",
+    src: "/data/fnt/PressStart2P.png",
+  },
+  {
+    name: "PressStart2P",
+    type: "binary",
+    src: "/data/fnt/PressStart2P.fnt",
+  },
+  {
+    name: "dude",
+    type: "image",
+    src: "/data/img/1 idle.png",
+  },
 ];
 
 export default DataManifest;
